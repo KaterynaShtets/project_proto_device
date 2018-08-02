db45cdecd685ff315f6a8d1229d2e3d6b869cca4
Katrinn23 <40269942+Katrinn23@users.noreply.github.com>
2018-08-02 16:41:28 +0300

    Update SmartHouse.js

--- js/SmartHouse.js
+++ js/SmartHouse.js
@@ -6,21 +6,32 @@ SmartHouse.prototype.setOwner = function (owner) {
     if (typeof owner == 'string') {
         this._owner = owner
     }
+}
     SmartHouse.prototype.getOwner = function () {
         return this._owner
     }
 
-    SmartHouse.prototype.addDevices = function () {
-        for (var i = 0; i < arguments.length; i++) {
-            this._devices.push(arguments[i])
+    SmartHouse.prototype.designDevice = function(type, name) {
+        var newDevice = null;
+        switch (type) {
+            case 'fridge':
+                newDevice = new Fridge(500, -2);
+                this._devices.push(newDevice);
+              
+                break;
+            case 'coffeemachine':
+                
+                newDevice = new CoffeeMachine(100000, 400);
+                this._devices.push(newDevice);
+                break;
+        
+        case 'coffeemachine':
+                
+                newDevice = new Microwave(300, 1000)
+                this._devices.push(newDevice);
+                break;
         }
     }
-}
-
-var smhouse = new SmartHouse('kate')
-var coffeeMachine = new CoffeeMachine(100000, 400);
-smhouse.addDevices(coffeeMachine);
-var fridge = new Fridge(500, -2);
-smhouse.addDevices(fridge)
-var microwave = new Microwave(300, 1000);
-smhouse.addDevices(microwave)
+    
+    
+        var sm=new SmartHouse('Kate')
