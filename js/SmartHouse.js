d1110e315f0f20ed1df40874fd7f12c525a47adb
Katrinn23 <40269942+Katrinn23@users.noreply.github.com>
2018-08-03 08:24:37 +0300

    Update SmartHouse.js

--- js/SmartHouse.js
+++ js/SmartHouse.js
@@ -11,27 +11,11 @@ SmartHouse.prototype.setOwner = function (owner) {
         return this._owner
     }
 
-    SmartHouse.prototype.designDevice = function(type, name) {
-        var newDevice = null;
-        switch (type) {
-            case 'fridge':
-                newDevice = new Fridge(500, -2);
-                this._devices.push(newDevice);
-              
-                break;
-            case 'coffeemachine':
-                
-                newDevice = new CoffeeMachine(100000, 400);
-                this._devices.push(newDevice);
-                break;
-        
-        case 'coffeemachine':
-                
-                newDevice = new Microwave(300, 1000)
-                this._devices.push(newDevice);
-                break;
+    
+        var sm=new SmartHouse('Kate')
+    SmartHouse.prototype.addDevices = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this._devices.push(arguments[i])
         }
     }
     
-    
-        var sm=new SmartHouse('Kate')
